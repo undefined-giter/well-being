511070d39d466cc929296972d74a6d8c51997b49
Lysard <leo.ripert@gmail.com>
2024-04-17 20:25:51 +0200

    :closed_lock_with_key:Password Update Done

--- src/Controller/UpdatePasswordController.php
+++ src/Controller/UpdatePasswordController.php
@@ -4,37 +4,50 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UpdatePasswordType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class UpdatePasswordController extends AbstractController
 {
+    private $passwordHasher;
+    private $entityManager;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager)
+    {
+        $this->passwordHasher = $passwordHasher;
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/update-password', name: 'update_password')]
     public function index(Request $request): Response
     {
         $user = $this->getUser();
 
-        if(!$user){return $this->redirectToRoute('login');}
+        if (!$user) {
+            return $this->redirectToRoute('login');
+        }
 
         $form = $this->createForm(UpdatePasswordType::class, $user);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
 
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
+            $hashedPassword = $this->passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($hashedPassword);
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
 
             $this->addFlash('success', 'Password updated successfully!');
 
-            $slug = $user->getSlug();
-
-            return $this->redirectToRoute('personal_page', [
-                'slug' => $slug,
-            ]);
+            return $this->redirectToRoute('personal_page', ['slug' => $this->getUser()->getSlug()]);
         }
 
         return $this->render('update_password/index.html.twig', [
